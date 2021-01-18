c7e7c8680aedf23119cfabed2a097be235269e1e
Abdellah Chadidi <chadidi97@gmail.com>
2021-01-18 22:34:55 +0100

    Test case setup & create test models & create first test

---
+++ tests/TranslationModelTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LaravelArab\Tarjama\Tests;
+
+class TranslationModelTest extends TestCase
+{
+    public function testCanCreateTranslation()
+    {
+        $this->assertTrue(true);
+    }
+}
